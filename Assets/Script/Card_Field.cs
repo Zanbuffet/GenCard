1ef7d8fcf627db1ffc03fd55998295f2fe68b9f6
Damendor <mrnegel@gmail.com>
2021-06-09 13:57:42 +0800

    FightingQueue+Damage

--- Assets/Script/Card_Field.cs
+++ Assets/Script/Card_Field.cs
@@ -8,16 +8,38 @@ public class Card_Field : MonoBehaviour
     public Card cardinfo;
     public int idx;
 
+    private int currentHP;
 
-    public void RefreshCard(Card cardinfo_) {
-        cardinfo=cardinfo_;
-        Debug.Log("Card Added! " +cardinfo.HP);
-        transform.Find("HP").GetComponent<Text>().text=""+cardinfo.cur_HP;
-        transform.Find("Image").GetComponent<Image>().sprite=cardinfo.icon_field;
+    public void RefreshCard(Card cardinfo_)
+    {
+        cardinfo = cardinfo_;
+        Debug.Log("Card Added! " + cardinfo.HP);
+        currentHP = cardinfo.cur_HP;
+        transform.Find("HP").GetComponent<Text>().text = "" + cardinfo.cur_HP;
+        transform.Find("Image").GetComponent<Image>().sprite = cardinfo.icon_field;
     }
 
+    public void TakeDamage(int damage)
+    {
+        currentHP -= damage;
+        UpdateCard();
+        StartCoroutine(TakingDamage());
+    }
+
+    IEnumerator TakingDamage()
+    {
+        GetComponent<Image>().color = Color.red;
+        yield return new WaitForSeconds(2f);
+        GetComponent<Image>().color = Color.white;
+    }
+
+    public void UpdateCard()
+    {
+        transform.Find("HP").GetComponent<Text>().text = "" + currentHP;
+        //transform.Find("Image").GetComponent<Image>().sprite = cardinfo.icon_field;
+    }
     public void OnSelect()
     {
-         GameObject.Find("UIManager").GetComponent<UIManager>().Place_card(idx);
+        GameObject.Find("UIManager").GetComponent<UIManager>().Place_card(idx);
     }
 }
---
+++ Assets/Script/Fight/FighManager.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class FighManager : MonoBehaviour
+{
+    //public List<Card> fightQueue = new List<Card>();
+    public Dictionary<int, Card> fightQueue = new Dictionary<int, Card>();
+    public List<GameObject> slots = new List<GameObject>();     //temp
+    public GameObject enemyBase;
+    public void InitFighting()
+    {
+        foreach (var slot in slots)
+        {
+            Card card = slot.GetComponent<Card_Field>().cardinfo;
+            if (card != null)
+            {
+                //slot.GetComponent<Card_Field>().RefreshCard(card);
+                fightQueue.Add(slot.GetComponent<Card_Field>().idx, card);
+            }
+        }
+    }
+
+    IEnumerator Fighting()
+    {
+        foreach (var card in fightQueue)
+        {
+            //temp effect
+            slots[card.Key].GetComponent<Image>().color = Color.green;
+            GameObject[] targets = GetTargets(card.Key).ToArray();
+            foreach (var tar in targets)
+            {
+                Debug.Log("Card " + card + "Attack " + tar.name + "  Damage: " + card.Value.HP);
+                Card_Field targetCard = tar.GetComponent<Card_Field>();
+                if (targetCard != null)
+                {
+                    targetCard.TakeDamage(card.Value.Attack);
+                }
+
+            }
+            //CardFight(fightQueue[i]);
+            yield return new WaitForSeconds(2f);
+            slots[card.Key].GetComponent<Image>().color = Color.white;
+        }
+    }
+
+
+    private List<GameObject> GetTargets(int idx)
+    {
+        List<GameObject> targets = new List<GameObject>();
+        if (idx > 0 && idx < slots.Count - 1)
+        {
+            if (fightQueue.ContainsKey(idx + 1))
+            {
+                //Card card = fightQueue[idx + 1];
+                targets.Add(slots[idx + 1]);
+            }
+            if (fightQueue.ContainsKey(idx - 1))
+            {
+                //Card card = fightQueue[idx + 1];
+                targets.Add(slots[idx - 1]);
+            }
+            if (targets.Count < 2)
+            {
+                targets.Add(enemyBase);
+            }
+        }
+        return targets;
+
+    }
+    public void InitButton()
+    {
+        //
+        InitFighting();
+        //
+    }
+    public void FightButton()
+    {
+        InitFighting();
+        StartCoroutine(Fighting());
+    }
+}
