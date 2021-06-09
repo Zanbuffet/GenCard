53281a9279908437f3a084abb2dbacca8b6a22c4
Zanbuffet <zanbuffet@126.com>
2021-06-10 02:33:24 +0800

    Something went wrong at my previous note

--- Assets/Script/Card_Group.cs
+++ Assets/Script/Card_Group.cs
@@ -11,14 +11,44 @@ public class Card_Group : MonoBehaviour
 
     public void RefreshCard(Card cardinfo_) {
         cardinfo=cardinfo_;
-        transform.Find("HP").GetComponent<Text>().text=""+cardinfo.HP;
-        transform.Find("cost").GetComponent<Text>().text=""+cardinfo.cost;
-        transform.Find("Image").GetComponent<Image>().sprite=cardinfo.icon_deck;
+        transform.Find("Container").Find("HP").GetComponent<Text>().text=""+cardinfo.HP;
+        transform.Find("Container").Find("cost").GetComponent<Text>().text=""+cardinfo.cost;
+        transform.Find("Container").Find("Image").GetComponent<Image>().sprite=cardinfo.icon_deck;
     }
 
     public void OnSelect(){
         // call ui
         GameObject.Find("UIManager").GetComponent<UIManager>().Select_card(idx);
     }
+    private bool isHover = false;
 
+    public void MouseOn(){
+        isHover = true;
+    }
+    public void MouseExit(){
+        isHover = false;
+    }
+    private Transform containerTransform;
+    private HorizontalLayoutGroup deckTransform;
+    private float originalSpacing;
+    public float horizontalDistance;
+    private Vector3 originalPos;
+    public  Vector3 distance;
+    public float moveSpeed;
+    private void Start() {
+        containerTransform = transform.Find("Container").GetComponent<RectTransform>();
+        originalPos = containerTransform.localPosition;
+        deckTransform = transform.parent.GetComponent<HorizontalLayoutGroup>();
+        originalSpacing = deckTransform.spacing;
+    }
+    public void Update(){
+        if(isHover){
+            containerTransform.localPosition = Vector3.Lerp(containerTransform.localPosition, originalPos+distance, moveSpeed * Time.deltaTime);
+            deckTransform.spacing = Mathf.Lerp(deckTransform.spacing, originalSpacing+ horizontalDistance,moveSpeed*Time.deltaTime );
+        }else{
+            containerTransform.localPosition = Vector3.Lerp(containerTransform.localPosition, originalPos, moveSpeed * Time.deltaTime);
+            deckTransform.spacing = Mathf.Lerp(deckTransform.spacing, originalSpacing,moveSpeed*Time.deltaTime );
+        }
+    }
+    
 }
